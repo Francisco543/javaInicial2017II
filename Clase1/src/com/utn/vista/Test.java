1276a03e2da44f5b5d23e0de338ae3530f13621e
Panella <524160@cognizant.com>
2017-08-01 22:13:33 -0300

    Clas1

---
+++ Clase1/src/com/utn/vista/Test.java
@@ -0,0 +1,52 @@
+package com.utn.vista;
+
+public class Test {
+	//Esta es mi primer clase de java
+	//voy a mostrar un hola mundo por la consola
+
+	/*
+	 * Esto   
+	 * Es 
+	 * Un 
+	 * COMENTARIO
+	 * GRANDOTE
+	 *  
+	 */
+
+	
+	//variables:
+	//			tipo de dato
+	//			valor 
+	
+	String nombre ="Dante"; 	//cadena de texto guardo el nombre
+	String apellido ="Panella"; //una cadena de texto guardo el apellido
+	char sexo='H'; 		//una sola letra o caracter
+	int edad=35; 		//es un numero entero
+	double peso=100.0; 	//numeros con decimales de mayor rango
+	float altura=194; 	//numeros con decimales de menor rango
+	long dni=001110011 ;		//un numero entero o con decimales 
+	
+	
+	//metodo main ( inicio de la aplicacion)
+	public static void main(String[] args) {
+		System.out.println("Hola pepito");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
